c7bf1d458f030448686a8ea5d56325656abc7022
Pedro Lima Silva <pdrolimasilva@gmail.com>
2026-08-06 22:47:19 -0300

    Log erro publico

--- Backend/src/routes/publicoRoutes.js
+++ Backend/src/routes/publicoRoutes.js
@@ -9,8 +9,10 @@ router.get("/home", async (req, res, next) => {
         const dados = await PublicoService.home();
         return res.status(200).json({ sucesso: true, ...dados });
     } catch (erro) {
-        return next(erro);
-    }
+    console.error("ERRO PUBLICO HOME:");
+    console.error(erro);
+    return next(erro);
+}
 });
 
 router.get("/vagas", async (req, res, next) => {
@@ -18,8 +20,10 @@ router.get("/vagas", async (req, res, next) => {
         const vagas = await PublicoService.vagasPublicas(req.query);
         return res.status(200).json({ sucesso: true, vagas });
     } catch (erro) {
-        return next(erro);
-    }
+    console.error("ERRO PUBLICO HOME:");
+    console.error(erro);
+    return next(erro);
+}
 });
 
 export default router;
